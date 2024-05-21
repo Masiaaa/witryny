089bf4ba9c723e2c3cdd5c137622dba5abb27f9f
Masiaaa <160600505+Masiaaa@users.noreply.github.com>
2024-05-21 08:45:43 +0200

    Update 1_podstawy_php.php

--- php/1_podstawy_php.php
+++ php/1_podstawy_php.php
@@ -11,5 +11,36 @@
 <?php
 print("PHP");
 ?>
+echo ('Zaczynam poznawać PHP');
+print ("<br>");
+echo ("Zaczynam poznawać PHP");
+print ("<br>");
+print ('Zaczynam poznawać PHP');
+print ("<br>");
+print ("Zaczynam poznawać PHP");
+print ("Zaczynam poznawać PHP. "."Lubię PHP.");
+<?php
+define ("SZKOLA", "ZSM");
+print (SZKOLA." to moja szkoła.");
+print ("Moja szkoła to ".SZKOLA.".");
+?>
+$zmienna="To jest zmienna typu string";
+$zmienna='To też jest zmienna typu string';
+$imie="Adam";
+$nazwisko="Kowalski";
+$tekst="Nazywam się $imie $nazwisko";
+print ($tekst);
+$tekst='Nazywam się $imie $nazwisko';
+print ($tekst);
+$liczba1=21;
+$liczba2=356;
+$liczba3=35.46;
+$liczba4 = $liczba1 + 2;
+$suma=$liczba1+$liczba2+$liczba3;
+print ($liczba4);
+print ($suma);
+
+
+	
 </body>
 </html>
