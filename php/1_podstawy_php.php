51be21383f72e116eea348ba8971963b3651e8a8
Masiaaa <160600505+Masiaaa@users.noreply.github.com>
2024-05-21 08:30:38 +0200

    Add files via upload

---
+++ php/1_podstawy_php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<title>moja galeria</title>
+	<meta name="keywords" content="szablon strony">
+	<meta name="description" content="Galeria">
+	<meta name="author" content="Maria Libera">
+</head>
+<body>
+</body>
+</html>
